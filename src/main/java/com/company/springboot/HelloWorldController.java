b16f9075138f1d3a6381550468f063d74f94d5fc
eslota1 <elisabethslota0411@gmail.com>
2021-02-02 22:37:04 -0500

    added my name, took out other stuff after Hello

--- src/main/java/com/company/springboot/HelloWorldController.java
+++ src/main/java/com/company/springboot/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
     @RequestMapping("/")
     public String index() {
-        return "Hello World from IntellJ!";
+        return "Hello Elisabeth!";
     }
 }
