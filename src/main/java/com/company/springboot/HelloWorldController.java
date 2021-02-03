bb8a2359f99f345cd16a22ac2f5f2d1147802c25
eslota1 <elisabethslota0411@gmail.com>
2021-02-02 21:42:26 -0500

    Display IDEA name on homepage

--- src/main/java/com/company/springboot/HelloWorldController.java
+++ src/main/java/com/company/springboot/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
     @RequestMapping("/")
     public String index() {
-        return "Hello World from IDE!!";
+        return "Hello World from IntellJ!";
     }
 }
